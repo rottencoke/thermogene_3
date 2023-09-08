696ca9f0356b96fbed52c0784df8788c5e55c4b7
rottencoke <berottencoke23@gmail.com>
2023-09-08 16:27:55 +0900

    Add import csv to render-build sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,3 +6,7 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+
+# tempura db構築
+require "./lib/assets/tempura/ruby/import_csv.rb"
+ImportCsv.execute(model: Tempura, file_name: "./db/csv_data/200617_TEMPURA")
