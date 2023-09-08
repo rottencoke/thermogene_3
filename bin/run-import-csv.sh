ef970823b0ef50aa3b19971c4829e49037be8bf7
rottencoke <berottencoke23@gmail.com>
2023-09-08 17:29:05 +0900

    Modify import csv to render-build sh #5

--- bin/run-import-csv.sh
+++ bin/run-import-csv.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# Railsアプリケーションの環境変数を設定
+export RAILS_ENV=production  
+
 # Rubyスクリプトを実行
-ruby -r "./lib/assets/tempura/ruby/import_csv.rb" -r "./app/models/tempura.rb" -e "ImportCsv.execute(model: Tempura, file_name: './db/csv_data/200617_TEMPURA')"
+ruby -r "./lib/assets/tempura/ruby/import_csv.rb" -r "./app/models/tempura.rb" -r "./app/models/application_record.rb" -e "ImportCsv.execute(model: Tempura, file_name: './db/csv_data/200617_TEMPURA')"
