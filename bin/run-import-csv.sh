e5fb0f1de192024c8a106587b98e061aec98aac7
rottencoke <berottencoke23@gmail.com>
2023-09-08 16:59:04 +0900

    Modify import csv to render-build sh #2

--- bin/run-import-csv.sh
+++ bin/run-import-csv.sh
@@ -2,4 +2,4 @@
 
 # TEMPURA DBの構築
 # Rubyコードの実行
-ruby -r "./lib/assets/tempura/ruby/import_csv.rb" -e "ImportCsv.execute(model: Tempura, file_name: './db/csv_data/200617_TEMPURA')"
+ruby -r "./lib/assets/tempura/ruby/import_csv.rb" -e "ImportCsv.execute(model: Tempura, file_name: '../db/csv_data/200617_TEMPURA')"
