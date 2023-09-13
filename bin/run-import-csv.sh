a68303e1276c5294ed7495ef735cfd6027c4a177
rottencoke <berottencoke23@gmail.com>
2023-09-13 21:36:45 +0900

    Add import_csv.rake

--- bin/run-import-csv.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Railsアプリケーションの環境変数を設定
-export RAILS_ENV=production  
-
-# Rubyスクリプトを実行
-ruby -r "./lib/assets/tempura/ruby/import_csv.rb" -r "./app/models/tempura.rb" -r "./app/models/application_record.rb" -e "ImportCsv.execute(model: Tempura, file_name: './db/csv_data/200617_TEMPURA')"
---
+++ lib/tasks/import_csv.rake
@@ -0,0 +1,7 @@
+namespace :import_csv do
+    desc "Import CSV data"
+    task tempura: :environment do
+        require './lib/assets/tempura/ruby/import_csv.rb'
+        ImportCsv.execute(model: Tempura, file_name: './db/csv_data/200617_TEMPURA')
+    end
+end
