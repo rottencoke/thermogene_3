8f352931eb12274a32f3aa5101c4caeaa86a1057
rottencoke <berottencoke23@gmail.com>
2023-11-01 00:12:17 +0900

    Update rare codons of E coli

--- app/javascript/functions/show_rare_codons/rare_codons.js
+++ app/javascript/functions/show_rare_codons/rare_codons.js
@@ -1,2 +1,2 @@
 // 大腸菌のレアコドン
-export const RARE_CODONS_E_COLI = ["AGA", "AGG", "AUA", "CUA", "GGA", "CCC", "CGG"];
+export const RARE_CODONS_E_COLI = ["AGA", "AGG", "ATA", "CTA", "CGA", "CCC", "CGG", "TGG"];
