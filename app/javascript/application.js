2a822bf49e4e192986361a23c2e4f3a0f7027b5b
rottencoke <berottencoke23@gmail.com>
2023-11-27 19:19:14 +0900

    Change files to load javascripts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,15 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// 自作javascriptファイルの保存
+import { show_rare_codons } from "show_rare_codons";
+import { render_searches } from "searches";
+import { control_sort } from "control_sort";
+import { control_filter } from "control_filter";
+import { render_results } from "render_results";
+await render_searches();
+await render_results();
+show_rare_codons();
+await control_sort();
+await control_filter();
